60f95d5b8b1c39c3978e2bada6ae1f00f1c744eb
eduardokaique99 <eduardo_kaique99@hotmail.com>
2023-08-21 22:07:10 -0300

    21/08/2023 - att 1

---
+++ config/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = 'localhost';
+$username = 'senac';
+$passowrd = 'senac';
+$database = 'aula';
+
+// criando conexão com o banco de dados 
+$pdo = new PDO("mysql:host=$host;dbname=$database", $username, $passowrd);
+// realizo um select para testar a conexão
+//$sql = "SELECT * FROM noticias";
+// executo o sql dentro do banco baseado nas informações da conexão
+//$resultado = $pdo->query($sql);
+//
+//if($resultado){
+//    echo 'Conexão realizada com sucesso!';
+//}
